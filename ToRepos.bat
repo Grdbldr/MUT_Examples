502f97eeec84b76201905d9d5d28ef486fbb75c0
Rob McLaren <robertmclaren231@gmail.com>
2026-08-07 10:44:19 -0400

    Version 2025.003 - Nodal CV catchment updates, observation/velocity layouts, and refreshed MUT binaries Enable nodal control volumes and revise critical-depth cells in 8_V_catchment_nc; add ConstantHead obs and hillslope velocity layouts; update Abdul/batch Tecplot posts and bump MUT_USERBIN executables.

---
+++ ToRepos.bat
@@ -0,0 +1,48 @@
+robocopy . C:\_repo\GrdBldr\MUT_Examples BatchComparisonPlots.lay Run.MUTBatch Run_debug.MUTBatch  ToRepos.bat
+
+robocopy .\1_VSF_Column C:\_repo\GrdBldr\MUT_Examples\1_VSF_Column  *.mut *.lay *.png
+robocopy .\1_VSF_Column\data C:\_repo\GrdBldr\MUT_Examples\1_VSF_Column\data
+
+robocopy .\1_VSF_Column_Brooks C:\_repo\GrdBldr\MUT_Examples\1_VSF_Column_Brooks  *.mut *.lay *.png
+robocopy .\1_VSF_Column_Brooks\data C:\_repo\GrdBldr\MUT_Examples\1_VSF_Column_Brooks\data
+
+robocopy .\2_VSF_Hillslope C:\_repo\GrdBldr\MUT_Examples\2_VSF_Hillslope  *.mut *.lay  *.png
+
+robocopy .\3_0_SWF_CHD C:\_repo\GrdBldr\MUT_Examples\3_0_SWF_CHD  *.mut *.lay  *.png
+
+robocopy .\3_1_CLN_for_SWF C:\_repo\GrdBldr\MUT_Examples\3_1_CLN_for_SWF  *.mut *.lay  *.png
+
+robocopy .\3_SWF C:\_repo\GrdBldr\MUT_Examples\3_SWF  *.mut *.lay  *.png
+
+robocopy .\4_SWF_RCH_CRD C:\_repo\GrdBldr\MUT_Examples\4_SWF_RCH_CRD  *.mut *.lay  *.png
+
+robocopy .\6_Abdul_Prism_Cell C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell  *.mut *.lay  custom*  *.png *.pptx
+robocopy .\6_Abdul_Prism_Cell\gb C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell\gb
+robocopy .\6_Abdul_Prism_Cell\data C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell\data
+
+robocopy .\6_Abdul_Prism_Cell_nc C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell_nc  *.mut *.lay  custom*  *.png
+robocopy .\6_Abdul_Prism_Cell_nc\gb C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell_nc\gb
+robocopy .\6_Abdul_Prism_Cell_nc\data C:\_repo\GrdBldr\MUT_Examples\6_Abdul_Prism_Cell_nc\data
+
+robocopy .\6_Abdul_MODHMS C:\_repo\GrdBldr\MUT_Examples\6_Abdul_MODHMS  *.mut *.lay custom*
+robocopy .\6_Abdul_MODHMS\TopElevation C:\_repo\GrdBldr\MUT_Examples\6_Abdul_MODHMS\TopElevation
+robocopy .\6_Abdul_MODHMS\IBound C:\_repo\GrdBldr\MUT_Examples\6_Abdul_MODHMS\IBound
+robocopy .\6_Abdul_MODHMS\data C:\_repo\GrdBldr\MUT_Examples\6_Abdul_MODHMS\data
+
+robocopy .\7_SuperSlab C:\_repo\GrdBldr\MUT_Examples\7_SuperSlab  *.mut *.lay custom* *.png
+
+robocopy .\8_V_catchment C:\_repo\GrdBldr\MUT_Examples\8_V_catchment  *.mut *.lay  custom*  *.png
+robocopy .\8_V_catchment\data C:\_repo\GrdBldr\MUT_Examples\8_V_catchment\data
+
+robocopy .\8_V_catchment_nc C:\_repo\GrdBldr\MUT_Examples\8_V_catchment_nc  *.mut *.lay  custom*  *.png
+robocopy .\8_V_catchment_nc\data C:\_repo\GrdBldr\MUT_Examples\8_V_catchment_nc\data
+
+robocopy .\9_Smith_Woolhiser C:\_repo\GrdBldr\MUT_Examples\9_Smith_Woolhiser  *.mut *.lay  custom*  *.png
+robocopy .\9_Smith_Woolhiser\data C:\_repo\GrdBldr\MUT_Examples\9_Smith_Woolhiser\data
+
+robocopy .\10_Forsyth C:\_repo\GrdBldr\MUT_Examples\10_Forsyth  *.mut *.lay  custom*  *.png
+robocopy .\10_Forsyth\data C:\_repo\GrdBldr\MUT_Examples\10_Forsyth\data
+
+robocopy .\11_Theis C:\_repo\GrdBldr\MUT_Examples\11_Theis  *.mut *.lay  custom*  *.png *.pptx
+robocopy .\11_Theis\gb C:\_repo\GrdBldr\MUT_Examples\11_Theis\gb
+robocopy .\11_Theis\data C:\_repo\GrdBldr\MUT_Examples\11_Theis\data
